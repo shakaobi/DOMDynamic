8b8ef6b7e402a56d1b3c374d7f6ae81ee1a187d0
Kyle Colangelo <kyle123c@hotmail.com>
2022-10-27 12:35:30 -0400

    new stuff

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+//A list that shows all of the user's added bookmarks.
+const bookmarkData = document.getElementById("addBookmark").value;
+
+const saveBookMark = [];
+
 document.getElementById("saveBtn").addEventListener("click", function() {
-  alert("Hello World!");
+  
+  //Get the value of the input field
+  const bookmarkData = document.getElementById("addBookmark").value;
+
+  //Create a new list item
+  const newBookmark = document.createElement("li");
+
+  //Add the bookmark to the list
+  newBookmark.innerHTML = bookmarkData;
+  console.log(bookmarkData);
+
+  saveBookMark.push(bookmarkData)
+  console.log(bookmarkData);
+
+  //Add the new bookmark to the list
+  document.getElementById("").append(newBookmark);
 });
+
+
+
+
+
+
+
